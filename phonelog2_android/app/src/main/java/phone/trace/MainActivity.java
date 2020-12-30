6050b91e7a054cbf005e195a5a0343d4e08f1698
Bertrand GUIRAL <bertrand.guiral@gmail.com>
2020-12-30 11:28:25 +0100

    bg

--- phonelog2_android/app/src/main/java/phone/trace/MainActivity.java
+++ phonelog2_android/app/src/main/java/phone/trace/MainActivity.java
@@ -2,24 +2,61 @@ package phone.trace;
 
 import phone.trace.db.AppAccountTable;
 import phone.trace.model.AppAccount;
+
+import android.Manifest;
 import android.accounts.Account;
 import android.accounts.AccountManager;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
 
-public class MainActivity extends AbstractActivityCrm {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
-    private String TAG = getClass().getSimpleName();
+import com.google.android.gms.instantapps.PackageManagerCompat;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String TAG = "bg "+getClass().getSimpleName();
+    private static final int REQUEST_RUNTIME_PERMISSION = 123;
     public static final String SERVICE_URL_CAFE_CRM = "http://phone-log.appspot.com/r/phonecall";
     ApplicationBg applicationBg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        String[] permissions = {
+                android.Manifest.permission.READ_CONTACTS,
+                android.Manifest.permission.WRITE_CONTACTS,
+                android.Manifest.permission.READ_CALENDAR,
+                android.Manifest.permission.WRITE_CALENDAR,
+                android.Manifest.permission.READ_CONTACTS,
+                android.Manifest.permission.READ_PHONE_STATE,
+                android.Manifest.permission.READ_CALL_LOG,
+                android.Manifest.permission.WRITE_CALL_LOG,
+                android.Manifest.permission.ACCESS_NETWORK_STATE,
+                android.Manifest.permission.INTERNET,
+                android.Manifest.permission.GET_ACCOUNTS,
+                android.Manifest.permission.ACCOUNT_MANAGER,
+                android.Manifest.permission.INTERNET,
+                android.Manifest.permission.READ_SYNC_SETTINGS,
+                android.Manifest.permission.WRITE_CONTACTS,
+                android.Manifest.permission.READ_CONTACTS,
+                android.Manifest.permission.READ_PHONE_STATE
+        };
+
+        String[] permissionsToRequest  = getListPermissionToRequest( permissions);
+        ActivityCompat.requestPermissions(this, permissionsToRequest, REQUEST_RUNTIME_PERMISSION);
+
 
         this.applicationBg = (ApplicationBg) this.getApplication();
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
@@ -55,9 +92,27 @@ public class MainActivity extends AbstractActivityCrm {
         startActivity(intent);
     }
 
+    private String[] getListPermissionToRequest( String[] permissions) {
+        List<String> lisPermissionRefused = new ArrayList<String>();
+        for (String permission : permissions){
+            if (!isGranted( permission)){
+                lisPermissionRefused.add(permission);
+            }
+        }
+        return toStringArray(lisPermissionRefused);
+    }
+    private boolean isGranted( String permission) {
+        return ContextCompat.checkSelfPermission(this,
+                permission) == PackageManager.PERMISSION_GRANTED;
+    }
 
-
-
-
+    // Function to convert ArrayList<String> to String[]
+    private static String[] toStringArray(List<String> list)    {
+        String strArray[] = new String[list.size()];
+        for (int j = 0; j < list.size(); j++) {
+            strArray[j] = list.get(j);
+        }
+        return strArray;
+    }
 
 }
